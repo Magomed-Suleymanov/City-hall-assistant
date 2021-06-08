7adb97e99467cb22ca5723983266bf663c4b230c
suleymanov <kinjudo@mail.ru>
2021-06-08 22:39:07 +0300

    слегка добавил инфы в db

--- server.js
+++ server.js
@@ -1,10 +1,26 @@
-const jsonServer = require('json-server')
-const server = jsonServer.create()
-const router = jsonServer.router('db.json')
-const middlewares = jsonServer.defaults()
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
+const users = router.db.get("users");
+const streets = router.db.get("streets");
 
-server.use(middlewares)
-server.use(router)
+server.use(middlewares);
+server.use(router);
 server.listen(3000, () => {
-    console.log('JSON Server is running')
-})
+  console.log("JSON Server is running");
+});
+
+//Авторизация
+server.route("/auth").post((req, res) => {
+  const { login, password } = req.body;
+  const authUser = users.find(
+    (user) => user.login === login && user.password === password
+  );
+  if (authUser.toJSON() === undefined) {
+    res.status(404).json("Ошибка авторизации");
+  }
+  res.json(authUser);
+});
+
+server.route("checkIn").post((req, res) => {});
