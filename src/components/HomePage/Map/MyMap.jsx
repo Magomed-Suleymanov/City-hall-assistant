8232329547b8ee511a6be1920301bf44e09718e3
suleymanov <kinjudo@mail.ru>
2021-06-13 14:39:07 +0300

    ...

--- src/components/HomePage/Map/MyMap.jsx
+++ src/components/HomePage/Map/MyMap.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
-import { Map, YMaps } from 'react-yandex-maps';
+import { GeoObject, Map, YMaps } from 'react-yandex-maps'
 import './style.css';
 
-function MyMap(props) {
-  const mapState = { center: [43.31868548726831, 45.6935787840332], zoom: 14 };
+function MyMap() {
+
+  const mapState = {
+    center: [43.31868548726831, 45.6935787840332], zoom: 14,
+    geometry: { type: "Point", coordinates: [43.31868548726831, 45.6935787840332]
+  }};
 
   return (
     <YMaps>
