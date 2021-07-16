a0eb5b253c766f4681cddabc07082c8d3a835e8f
Ismail <ismtim095@gmail.com>
2021-07-16 16:27:04 +0300

    Добавление карты и исправление бага

--- src/redux/actions/auth.js
+++ src/redux/actions/auth.js
@@ -84,3 +84,26 @@ export const loadStreets = () => {
       });
   };
 };
+
+export const addStreet = (address, latitude, longitude) => {
+  return dispatch => {
+    dispatch({type: 'add/street/load'})
+
+    fetch('/streets', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        address,
+        latitude,
+        longitude,
+      }),
+    })
+        .then(res => res.json())
+        .then(json => {
+          dispatch({
+            type: 'add/street/success',
+            payload: json,
+          })
+        })
+  }
+}
